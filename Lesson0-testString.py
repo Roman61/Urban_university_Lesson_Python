6c271e09a0099e8683c1d8d0fb09351f6f8ce944
Roman61 <Orodunaar@mail.ru>
2024-04-16 21:19:09 +0300

    Initial commit

---
+++ Lesson0-testString.py
@@ -0,0 +1,8 @@
+ThisIsMyString = 'это моя строка'
+ThisIsNewString = 'это новая строка'
+print(ThisIsMyString[0:1])
+print(ThisIsMyString[:len(ThisIsMyString)-2:-1])
+print(ThisIsMyString[2:5])
+print(ThisIsMyString[::-1])
+print(len(ThisIsMyString))
+print(ThisIsMyString+ThisIsNewString)

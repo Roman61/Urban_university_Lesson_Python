c9a74e4010b11abdbc507366f6a464cb5bd349a0
Roman61 <Orodunaar@mail.ru>
2024-04-18 20:20:56 +0300

    Динамическая типизация

---
+++ HomeWorks/Module-1/homework2.py
@@ -0,0 +1,8 @@
+name = input("Введите ваше имя: ")
+print("Здраствуйте, " + name + "!")
+age = int(input("Введите ваш возраст: "))
+print(age)
+age += 1
+print("В следующем году вам исполнится: " + str(age))
+is_student = input("Явлетесь ли Вы студентом?(True/False) - ")
+print(is_student)

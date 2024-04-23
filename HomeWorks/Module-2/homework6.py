7668087900ef1494c42ec34552d8241342f652e3
Roman61 <Orodunaar@mail.ru>
2024-04-23 18:48:17 +0300

    Функции в Python.Функция с параметром

---
+++ HomeWorks/Module-2/homework6.py
@@ -0,0 +1,14 @@
+
+list_ = ["First", "Second"]
+dict_ = {"first": 11, "second": 22}
+
+
+def print_params(first, second):
+    print(second)
+
+
+print_params("FIRST", "SECOND")
+print_params(1, 2)
+print_params(*list_)
+print_params(**dict_)
+pass

35101f4e8aca546d0d38bad5e87bcd463765a62d
Roman61 <Orodunaar@mail.ru>
2024-04-23 18:59:07 +0300

    Функции в Python.Функция с параметром 3

--- HomeWorks/Module-2/homework6.py
+++ HomeWorks/Module-2/homework6.py
@@ -1,14 +1,14 @@
 
-list_ = ["First", "Second"]
-dict_ = {"first": 11, "second": 22}
+list_ = ["Second"]
+dict_ = {"second": 22}
 
 
-def print_params(first, second):
+def print_params(second):
     print(str(second)*2)
 
 
-print_params("FIRST", "SECOND")
-print_params(1, 2)
+print_params("SECOND")
+print_params(2)
 print_params(*list_)
 print_params(**dict_)
 pass

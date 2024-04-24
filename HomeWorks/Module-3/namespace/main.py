8982c78e74d8eadd4a93350e32871567023fbb94
Roman61 <Orodunaar@mail.ru>
2024-04-24 20:17:18 +0300

    Пространство имен и способы вызова функции

---
+++ HomeWorks/Module-3/namespace/main.py
@@ -0,0 +1,16 @@
+def test():
+    a = 1
+    b = 2
+    print(a, b)
+
+
+def test2(bl, flg, Hrz):
+    print(bl, flg, Hrz)
+
+
+blob = "blob"
+flag = False
+Hz = 123
+
+test()
+test2(blob, flag, Hz)

14f2190f2a82584e0715f82260a6248785794069
Roman61 <Orodunaar@mail.ru>
2024-05-11 21:13:12 +0300

    threading Thread list

--- HomeWorks/Lesson_my/Threading/Python_1.py
+++ HomeWorks/Lesson_my/Threading/Python_1.py
@@ -2,25 +2,37 @@ import threading
 import time
 
 
-def get_data(thr_name, data):
-    for i in range(100):
-        print(f'{thr_name} - {i} - {data}')
-        time.sleep(5)
+def get_data(data, value):
+    for _ in range(value):
+        print(f'[{threading.current_thread().name}] - {data}')
+        time.sleep(1)
 
 
-thr = threading.Thread(group=None, target=get_data,
-                       name="thr-1", args=(str(time.time()), 1),
-                       kwargs={}, daemon=None)
+thr_list = []
 
-# thr = threading.Thread(target=get_data, args=(str(time()),), name="thr-1")
-thr.start()
+for i in range(3):
+    thr = threading.Thread(group=None, target=get_data,
+                           name=f"thr-{i}", args=(str(time.time()), i,),
+                           kwargs={}, daemon=None)
+    thr_list.append(thr)
+    # thr = threading.Thread(target=get_data, args=(str(time()),), name="thr-1")
+    thr.start()
 
-for i in range(100):
-    print(f"current: {i}")
-    time.sleep(1)
+print(thr_list)
 
-    if i % 10 == 0:
-        print("active tread:", threading.active_count())
-        print("enumerate:", threading.enumerate())
-        print("thr-1 is alive:", thr.is_alive())
+for i in thr_list:
+    i.join()
 
+print("finish")
+# print("Name", threading.main_thread().name)
+# threading.main_thread().setName("resault")
+# print("resault", threading.main_thread().name)
+
+# for i in range(100):
+#     print(f"current: {i}")
+#     time.sleep(1)
+#
+#     if i % 10 == 0:
+#         print("active tread:", threading.active_count())
+#         print("enumerate:", threading.enumerate())
+#         print("thr-1 is alive:", thr.is_alive())

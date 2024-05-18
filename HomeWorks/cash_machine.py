0f9ef6d49a34bba07141203055167f22034b1550
Roman61 <Orodunaar@mail.ru>
2024-05-18 19:08:38 +0300

    Ваша касса

--- HomeWorks/cash_machine.py
+++ HomeWorks/cash_machine.py
@@ -1,17 +1,9 @@
-from rich.console import Console
-from rich.panel import Panel
-from rich.text import Text
-from rich.prompt import Confirm, IntPrompt
-from rich.columns import Columns
+class PaymentMachine:
+    # конструктор
+    def __init__(self, name, age):
+        self.name = name  # имя человека
+        self.age = age  # возраст человека
+        print("Создание объекта Person")
 
-
-def func():
-    n = int(input("Введите число: "))
-    for i in range(1, n + 1):
-        # list_x[:] = [f(tup) for tup in zip(list_x, list_y)]
-        f = lambda: int(input("Введите числа: ").split(' '))
-        num = [f for j in range(1, x)]
-        print(num)
-
-
-func()
+    def say_hello(self):
+        print("Hello" + self.name)

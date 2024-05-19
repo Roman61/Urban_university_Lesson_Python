44beaabab54848f63f77e5280a5a9bc4b78941a5
Roman61 <Orodunaar@mail.ru>
2024-05-19 16:41:38 +0300

    Модули и пакеты

---
+++ HomeWorks/Module-4/homework1.py
@@ -0,0 +1,11 @@
+from homework1_1 import func as fn1
+from homework1_1 import func_1 as fn2
+
+
+def main():
+    fn1()
+    fn2()
+
+
+if __name__ == '__main__':
+    main()
---
+++ HomeWorks/Module-4/homework1_1.py
@@ -0,0 +1,7 @@
+def func():
+    print("Hello, world")
+
+
+def func_1():
+    print(f"Hello, world")
+

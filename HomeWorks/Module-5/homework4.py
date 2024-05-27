61c7c73642272bd9909e664c45d7e8526184aa5d
R.A.Korolev <Orodunaar@mail.ru>
2024-05-27 23:10:08 +0300

    Домашняя работа по уроку "Различие атрибутов класса и экземпляра."

--- HomeWorks/Module-5/homework4.py
+++ HomeWorks/Module-5/homework4.py
@@ -3,14 +3,14 @@
 # объектов класса Building total (по примеру класса Lemming из урока)
 # В цикле создайте 40 объектов класса Building и выведите их на экран командой print
 # Полученный код напишите в ответ к домашнему заданию
-total = 0
 
 
 class Buiding:
+    total = 0
+
     def __init__(self):
-        global total
-        total += 1
-        print(f'Объектов создано {total}')
+        Buiding.total += 1
+        print(f'Объектов создано {Buiding.total}')
 
 
 def main():

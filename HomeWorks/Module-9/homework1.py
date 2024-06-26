6b8150980e5d5ea98b35ffe23f51ded0878595b4
R.A.Korolev <Orodunaar@mail.ru>
2024-06-26 21:09:05 +0300

    Списковые, словарные сборки

---
+++ HomeWorks/Module-9/homework1.py
@@ -0,0 +1,20 @@
+
+def is_even(num):
+    if num % 2 != 0:
+        return True
+    else:
+        return False
+
+
+def square(num):
+    return num ** 2
+
+
+arr = [1, 2, 5, 7, 12, 11, 35, 4, 89, 10]
+
+thinning_arr = list(filter(is_even, arr))
+out_arr = list(map(square, thinning_arr))
+print("Применение filter и map:\t\t\t\t", out_arr)
+
+out_arr_g = [x ** 2 for x in arr if x % 2 != 0]
+print("Применение списковые, словарные сборки:\t", out_arr_g)

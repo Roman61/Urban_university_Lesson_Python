d96ea92253e9cb8d7a221cffefd5ae3e8feaae9c
R.A.Korolev <Orodunaar@mail.ru>
2024-07-27 04:16:39 +0300

    Домашнее задание по теме "Обзор сторонних библиотек Python Pillow

--- HomeWorks/Module-11/homework1_pillow.py
+++ HomeWorks/Module-11/homework1_pillow.py
@@ -2,6 +2,18 @@ from PIL import Image
 
 image_path = 'H:/Image/8blmpyIqFQ4.jpg'
 
+img = Image.open(image_path)
+# получаем ширину и высоту
+width, height = img.size
+print(width, height)
+# открываем картинку в окне
+img.show()
+
+img = Image.open(image_path)
+# изменяем размер
+new_image = img.resize((200, 385))
+new_image.show()
+
 # указываем фиксированный размер стороны
 fixed_width = 200
 img = Image.open(image_path)
@@ -15,3 +27,21 @@ height_size = int((float(img.size[0]) * float(width_percent)))
 new_image = img.resize((fixed_width, height_size))
 new_image.show()
 new_image.save('H:/Image/8blmpyIqFQ4-2.jpg')
+
+img = Image.open(image_path)
+size = img.size
+width, height = img.size
+# обрезаем картинку
+new_image = img.crop((0, 0, width, height - 60))
+new_image.show()
+new_image.save('H:/Image/8blmpyIqFQ4-3.jpg')
+
+old_img = Image.open(image_path)
+# создание нового изображения
+new_image = Image.new(old_img.mode,
+                      (old_img.size[0] + 50, old_img.size[1] + 50),
+                      'white')
+# вставляем старой изображение в новое
+new_image.paste(old_img, (25, 25))
+new_image.show()
+new_image.save('H:/Image/8blmpyIqFQ4-4.jpg')

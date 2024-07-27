22fc963797c509ddbee6558df1c1f471d75ecae0
R.A.Korolev <Orodunaar@mail.ru>
2024-07-27 03:48:08 +0300

    Домашнее задание по теме "Обзор сторонних библиотек Python numpy

---
+++ HomeWorks/Module-11/homework1_numpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+print(arr)
+arr = np.sqrt(arr)
+print(arr)
+arr = np.sin(arr)
+print(arr)
+arr = np.cos(arr)
+print(arr)
+arr = np.log(arr)
+print(arr)
+arr = np.exp(arr)
+print(arr)

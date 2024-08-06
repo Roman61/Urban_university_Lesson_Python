ec1319c6221e2c23d339028fca4dde81d383db71
R.A.Korolev <Orodunaar@mail.ru>
2024-08-06 21:06:04 +0300

    Домашнее задание по теме "Простые Юнит-Тесты"

---
+++ HomeWorks/Module-12/homework1.py
@@ -0,0 +1,33 @@
+from runner import Runner as Runner_1
+from runner_and_tournament import Tournament as Tournament_1
+from runner_and_tournament import Runner as Runner_2
+from rt_with_exceptions import Runner as Runner_3
+from rt_with_exceptions import Tournament as Tournament_2
+from unittest import TestCase
+from unittest import main
+
+
+class RunnerTest(TestCase):
+    def test_walk(self):
+        runner = Runner_1("Runner")
+        for i in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner_2("Runner")
+        for i in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner_2("Runner")
+        runner2 = Runner_3("Runner 1")
+        for i in range(10):
+            runner1.run()
+            runner2.walk()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
+if __name__ == "__main__":
+    main()
